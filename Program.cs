bb67a8ed9721e2ad6dd47f2f03eb7a5a00be442f
Larisa <larisa@kozlova.ru>
2023-08-14 02:32:14 +0300

    Добавила папку homework_4

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число
+// А в натуральную степень В.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("Введите число A:");
+//         int a = Convert.ToInt32(Console.ReadLine());
+        
+//         Console.WriteLine("Введите число B:");
+//         int b = Convert.ToInt32(Console.ReadLine());
+        
+//         int result = 1;
+        
+//         for(int i = 0; i < b; i++)
+//         {
+//             result *= a;
+//         }
+        
+//         Console.WriteLine("Результат: " + result);
+
+
+// Задача 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//  Console.WriteLine("Введите число:");
+//         int number = Convert.ToInt32(Console.ReadLine());
+        
+//         int sum = 0;
+        
+//         while (number > 0)
+//         {
+//             int digit = number % 10;
+//             sum += digit;
+//             number /= 10;
+//         }
+        
+//         Console.WriteLine("Сумма цифр: " + sum);
+
+
+// Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+   // Создаем массив из 8 элементов
+        // int[] array = new int[8] { 1, 2, 5, 7, 19, 6, 1, 33 };
+        
+        // // Выводим элементы массива на экран
+        // Console.Write("[");
+        // for (int i = 0; i < array.Length; i++)
+        // {
+        //     Console.Write(array[i]);
+            
+        //     // Если элемент не последний, то выводим запятую и пробел
+        //     if (i < array.Length - 1)
+        //     {
+        //         Console.Write(", ");
+        //     }
+        // }
+        // Console.WriteLine("]");
+    
